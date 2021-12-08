6bc1f7f8aa1dcfc0ab91885dc3ecee0d570fa937
Bhuvi-603741 <95731664+Bhuvi-603741@users.noreply.github.com>
2021-12-08 09:55:31 +0530

    Add files via upload

---
+++ inivisible_cloak.py
@@ -0,0 +1,48 @@
+#import liberaries
+import cv2 # for image processing
+import numpy as np #mathematical library for image handling
+import time
+
+cap = cv2.VideoCapture(0) #this is my webcam
+background = cv2.imread('./image.jpg')
+
+while cap.isOpened():
+    #caturing the live frame
+    ret, current_frame = cap.read()
+    if ret:
+        #converting from rgb to hsv color space
+        hsv_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2HSV)
+
+        #range for lower red and upper red
+        l_red = np.array([0,120,170])
+        u_red = np.array([10,255,255])
+        mask1 = cv2.inRange(hsv_frame, l_red, u_red)
+
+        #range for lower red and upper red
+        l_red = np.array([170,120,70])
+        u_red = np.array([180,255,255])
+        mask2 = cv2.inRange(hsv_frame, l_red, u_red)
+
+        #generating the final red mask
+        red_mask = mask1 + mask2
+        #after combining the mask we are storing the value in default mask
+
+        red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, np.ones((3,3), np.uint8), iterations = 10) 
+        red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_DILATE, np.ones((3,3), np.uint8), iterations = 1)  
+
+        #subsituting the red portion with backgrpound image
+        part1 = cv2.bitwise_and(background, background, mask= red_mask)
+        
+        # detecting things which are not red
+        red_free = cv2.bitwise_not(red_mask)
+
+        # if cloak is not present show the current image
+        part2 = cv2.bitwise_and(current_frame, current_frame, mask= red_free)
+
+
+        #final output
+        cv2.imshow("cloak", part1 + part2)
+        if cv2.waitKey(5) == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
